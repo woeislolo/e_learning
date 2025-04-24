7c33c00655a03857670681344e5dedef1c9456bc
woeislolo <falloutgirl7@mail.ru>
2025-04-24 15:30:36 +0500

    feat: add list view for courses

--- src/educa/courses/views.py
+++ src/educa/courses/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Course
+
+
+class OwnerMixin:
+    """ Фильтрует queryset объектов, принадлежащих юзеру """
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(owner=self.request.user)
+
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
