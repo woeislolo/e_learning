420c376fbbaa8be22fef3a3062bcf06ce075bf88
woeislolo <falloutgirl7@mail.ru>
2025-04-25 13:45:47 +0500

    feat: add create, update, delete a course views and templates, css and urls for them

--- src/educa/courses/views.py
+++ src/educa/courses/views.py
@@ -1,4 +1,9 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, \
+                                      DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+                                       PermissionRequiredMixin
+from django.urls import reverse_lazy
 
 from .models import Course
 
@@ -10,10 +15,42 @@ class OwnerMixin:
         return queryset.filter(owner=self.request.user)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerEditMixin:
+    """ Добавляет в объект текущего юзера как owner'а """
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    """ Использует шаблон формы для представлений CourseCreateView и CourseUpdateView """
+    template_name = 'courses/manage/course/form.html'
+    context_object_name = 'course'
 
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+    context_object_name = 'courses'
+
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
+    context_object_name = 'course'
