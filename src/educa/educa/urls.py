8f1fa011e6821e94556999fcea9b2ae81a4bf84c
woeislolo <falloutgirl7@mail.ru>
2025-04-28 15:38:13 +0500

    refactor: alter url (courses -> course)

--- src/educa/educa/urls.py
+++ src/educa/educa/urls.py
@@ -8,7 +8,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('courses/', include('courses.urls')),
+    # TODO: rename plural accounts
+    path('course/', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
 
