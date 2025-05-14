2b50a33325a2fd13a4add014c40cb2467d61a592
woeislolo <falloutgirl7@mail.ru>
2025-05-14 13:43:47 +0500

    feat: add view and template to registrate a student

--- src/educa/students/views.py
+++ src/educa/students/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+class StudentRegistrationView(CreateView):
+    """ Регистрирует нового студента, в случае успеха - редирект на главную страницу """
+    template_name = 'students/student/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('course_list')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd['username'],
+                            password=cd['password1'])
+        login(self.request, user)
+        return result
