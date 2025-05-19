b0749264028c2335f6a7e2dfdc57b8d66446f537
woeislolo <falloutgirl7@mail.ru>
2025-05-19 11:55:11 +0500

    feat: add cache (StudentCourseDetailView)

--- src/educa/students/urls.py
+++ src/educa/students/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 
@@ -7,6 +8,8 @@ urlpatterns = [
     path('register/', views.StudentRegistrationView.as_view(), name='student_registration'),
     path('enroll-course/', views.StudentEnrollCourseView.as_view(), name='student_enroll_course'),
     path('courses/', views.StudentCourseListView.as_view(), name='student_course_list'),
-    path('course/<pk>/', views.StudentCourseDetailView.as_view(), name='student_course_detail'),
-    path('course/<pk>/<module_id>/', views.StudentCourseDetailView.as_view(), name='student_course_detail_module'),
+    path('course/<pk>/', cache_page(60 * 15)(views.StudentCourseDetailView.as_view()), 
+         name='student_course_detail'),
+    path('course/<pk>/<module_id>/', cache_page(60 * 15)(views.StudentCourseDetailView.as_view()), 
+         name='student_course_detail_module'),
 ]
