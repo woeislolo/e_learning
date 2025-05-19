57d05b1ae9b1c4090ef7da683c8ae129d33999d0
woeislolo <falloutgirl7@mail.ru>
2025-05-19 11:00:58 +0500

    feat: add cache (subjects, courses) in CourseListView

--- src/educa/courses/views.py
+++ src/educa/courses/views.py
@@ -4,11 +4,12 @@ from django.views.generic import ListView, DetailView, \
     CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, \
                                        PermissionRequiredMixin
+from django.contrib.auth import logout
 from django.urls import reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
 from django.db.models import Count
-from django.contrib.auth import logout
+from django.core.cache import cache
 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
@@ -201,18 +202,33 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
 
 
 class CourseListView(TemplateResponseMixin, View):
-    """ Отображает все курсы либо курсы по предмету, переданному через параметр """
+    """ Отображает все курсы либо курсы по дисциплине, переданной через параметр.
+     Кэширует: дисциплины, курсы """
     model = Course
     template_name = 'courses/course/list.html'
 
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(
-                        total_courses=Count('courses'))
-        courses = Course.objects.annotate(
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                            total_courses=Count('courses'))
+            cache.set('all_subjects', subjects)
+
+        all_courses = Course.objects.annotate(
                         total_modules=Count('modules'))
         if subject:
             subject = get_object_or_404(Subject, slug=subject)
-            courses = courses.filter(subject=subject)
+            key = f'subject_{subject.id}_courses'
+            courses = cache.get(key)
+            if not courses:
+                courses = all_courses.filter(subject=subject)
+                cache.set(key, courses)
+        else:
+            courses = cache.get('all_courses')
+            if not courses:
+                courses = all_courses
+                cache.set('all_courses', courses)
+                
         return self.render_to_response({'subjects': subjects,
                                         'subject': subject,
                                         'courses': courses})
