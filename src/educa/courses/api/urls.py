f92cd80ad520e3663db4372f45f49e445deada95
woeislolo <falloutgirl7@mail.ru>
2025-05-21 15:09:12 +0500

    feat: add url for course viewset

--- src/educa/courses/api/urls.py
+++ src/educa/courses/api/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
 
 from .views import *
 
 
 app_name = 'courses'
 
+router = routers.DefaultRouter()
+router.register('courses', CourseViewSet)
+
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
+    path('', include(router.urls)), 
+        # courses/ (list), 
+        # courses/pk/ (retrieve), 
+        # courses/pk/enroll/ (enroll)
 ]
