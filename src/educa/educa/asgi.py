31aa1e1ae805d10cd3aa6ce5bf53b91379e917da
woeislolo <falloutgirl7@mail.ru>
2025-05-23 10:12:41 +0500

    feat: update settings in asgi.py

--- src/educa/educa/asgi.py
+++ src/educa/educa/asgi.py
@@ -11,6 +11,17 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
+
