9e6ecd4ee1279bfd4c2d9002f03ac5bda39c638f
woeislolo <falloutgirl7@mail.ru>
2025-05-23 10:22:33 +0500

    feat: add view for chat room

--- src/educa/chat/views.py
+++ src/educa/chat/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        course = request.user.courses_joined.get(id=course_id)
+    except:
+        return HttpResponseForbidden()
+    return render(request, 'chat/room.html', {'course': course})
