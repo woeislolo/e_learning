1ea5f8c0ba74e5cd4d75db7a8bfa3b9ba6a0e2b0
woeislolo <falloutgirl7@mail.ru>
2025-05-23 10:26:45 +0500

    feat: update asgi.py with settings for websocket

--- src/educa/educa/asgi.py
+++ src/educa/educa/asgi.py
@@ -11,17 +11,21 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+import chat.routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
-    ),
-})
-
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_app,
+        'websocket': AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+            ),
+    }
+)
